56d8cda530f908fa889ba2d012df1ae7fde965b9
Kerrigan <ggthjew4@gmail.com>
2019-03-23 17:11:09 +0800

    1.created new models for articles and users 2.introduced Devise to the project

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'pages#index'
+  root to: 'pages#index'
+  get '/about', to: 'pages#about'
 end
