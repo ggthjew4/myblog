f6477c006ce00890d4a6aa62b7f8afd5c021ec6c
Kerrigan <ggthjew4@gmail.com>
2019-03-28 08:22:20 +0800

    added checking that users is able to update article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,6 +13,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
+    @article.user_id = current_user.id
     if @article.save
       flash[:notice] = "Article was successfully created"
       redirect_to article_path(@article)
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,8 @@
 module ArticlesHelper
+
+  def allow_article_edit_delete?(article)
+    return false unless current_user
+    article.user_id == current_user.id
+  end
+
 end
